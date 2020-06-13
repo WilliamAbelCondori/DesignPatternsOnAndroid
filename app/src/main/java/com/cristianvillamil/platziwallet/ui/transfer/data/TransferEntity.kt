c37df1a4beceacf8aa7535ce51bdbc3f17911420
WilliamAbelCondori <1williamcondori1@gmail.com>
2020-06-12 20:04:29 -0400

    Room ORM

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/transfer/data/TransferEntity.kt
@@ -0,0 +1,25 @@
+package com.cristianvillamil.platziwallet.ui.transfer.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+// me creara una tabla llamada transfers con los parametros que hay dentro
+@Entity(tableName = "transfers")
+data class TransferEntity(
+    @ColumnInfo(name = "transaction_id")
+    @PrimaryKey(autoGenerate = true)
+    val transactionId: Int = 0,
+
+    @ColumnInfo(name = "user_id")
+    val userId : String,
+    @ColumnInfo(name = "user_name")
+    val userName: String,
+    @ColumnInfo(name = "transaction_date")
+    val transactionDate: String,
+    @ColumnInfo(name = "transaction_amount")
+    val transactionAmount: String,
+    @ColumnInfo(name = "receiver_user_id")
+    val receiverUserId:String
+)
