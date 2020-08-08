3ecb5830ed2e5834d2c3afb002a5d0f27cf34711
WilliamAbelCondori <1williamcondori1@gmail.com>
2020-08-08 15:52:41 -0400

    fixed

---
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/PercentageView.kt
@@ -0,0 +1,32 @@
+package com.cristianvillamil.platziwallet.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.cristianvillamil.platziwallet.R
+
+class PercentageView: FrameLayout{
+
+    constructor(context:Context, attributeSet: AttributeSet, defStyle: Int): super(
+        context,
+        attributeSet,
+        defStyle
+    ){
+        initView()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet){
+        initView()
+    }
+
+    constructor(context: Context):super(context){
+        initView()
+    }
+
+    private fun initView(){
+        val view = View.inflate(context, R.layout.percentage_view, null)
+        addView(view)
+
+    }
+}
